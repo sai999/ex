8e6467a5f2bf9fd1e3100814ee5a78bd738b5913
sai999 <sai9.kalluri@gmail.com>
2013-04-23 12:20:46 +0530

    first time commit

---
+++ src/com/one/Main.java
@@ -0,0 +1,23 @@
+package com.one;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try{
+			  BufferedReader object = new BufferedReader(new InputStreamReader(System.in));
+			  System.out.println("enter the number");
+			  int a= Integer.parseInt(object.readLine());
+			  int fact= 1;
+			  System.out.println("Factorial of " +a+ ":");
+			  for (int i= 1; i<=a; i++){
+			  fact=fact*i;
+			  }
+			  System.out.println(fact);
+			  }
+			  catch (Exception e){}
+
+	}
+
+}
